e887f4260847717cb0edc6357a49392d3b4df32e
Anuj Raghuvanshi <itsanujraghuvanshi@gmail.com>
2022-02-03 02:34:04 +0530

    Added Skills - In Progress

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -14,7 +14,7 @@ const Skills = () => {
 		<Section title="Skills">
 			<div className="skills-content">
 				<Fade duration={1000}>
-					<div style={{ position: 'relative', width: '100%', maxWidth: 600 }}>
+					<div style={{ position: 'relative', width: '100%', maxWidth: '90%' }}>
 						<IsVisible once>
 							{(isVisibleSkillsWrapper) => (
 								<div
@@ -30,24 +30,10 @@ const Skills = () => {
 									}
 								>
 									{/* <h2>Skills</h2> */}
-									<ul className="skills" ref={skillsWrapper}>
+									<ul className="skills" ref={skillsWrapper} sty>
 										{skills.map((skills) => {
 											return (
 												<li className="skill-bar-wrapper" key={skills.skillName}>
-													<div
-														className="skill-bar"
-														style={
-															isVisibleSkillsWrapper
-																? {
-																	transition: `${1 + skills.id / 10
-																		}s width ease-in-out`,
-																	width: width * (skills.amount / 100),
-																}
-																: {
-																	width: 1,
-																}
-														}
-													></div>
 													<div className="skill-name">{skills.skillName}</div>
 												</li>
 											)
