3aefa0d3545d7a62ba9233449fe62b9e0b006d8b
Anuj Raghuuvanshi <itsanujraghuvanshi@gmail.com>
2022-02-05 21:00:28 +0530

    Changes in skills

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -71,13 +71,6 @@ const ContactForm = () => {
 
   const classes = useStyles()
 
-  const validateEmail = (email) => {
-    return email.match(
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-  };
-  
-
   const submitForm = (ev) => {
     ev.preventDefault()
     console.log('submitForm', emailText, messageText)
