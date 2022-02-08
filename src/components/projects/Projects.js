8128028126acd5d85188f25a6df4ee6c08243a89
Anuj Raghuvanshi <itsanujraghuvanshi@gmail.com>
2022-02-09 01:44:13 +0530

    Removed Extra code

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,13 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './Projects.css'
 import { Fade } from 'react-reveal'
-import ApolloClient, { gql } from 'apollo-boost'
-import Button from '@material-ui/core/Button'
-import { makeStyles } from '@material-ui/core/styles'
 import { featured_projects } from '../../data/featured_projects.json'
 import Project from '../project/Project'
 import Section from '../section/Section'
-import FeaturedProject from '../featuredProject/FeaturedProject'
 
 const Projects = () => {
 
