4e2cc16ee4f74b1568d3e0cb64346d13509f2d7b
Anuj Raghuvanshi <22232709+anujraghuvanshi@users.noreply.github.com>
2023-07-23 03:46:50 +0530

    PWA Support

--- src/index.js
+++ src/index.js
@@ -3,4 +3,18 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('./service-worker.js').then(function (registration) {
+      console.log('Worker registration successful', registration.scope);
+    }, function (err) {
+      console.log('Worker registration failed', err);
+    }).catch(function (err) {
+      console.log(err);
+    });
+  });
+} else {
+  console.log('Service Worker is not supported by browser.');
+}
+
 ReactDOM.render(<App />, document.getElementById('root'))
