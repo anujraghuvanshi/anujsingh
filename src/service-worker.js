d66cc3fe5b1f8602668bc0f08bfa3e2b0c5fca73
Anuj Raghuvanshi <itsanujraghuvanshi@gmail.com>
2023-07-23 04:14:55 +0530

    Pwa (#18)
    * PWA Support
    
    * Update manifest.json
    
    * Update manifest.json
    
    * Update manifest.json

---
+++ src/service-worker.js
@@ -0,0 +1,65 @@
+// service-worker.js
+
+const CACHE_NAME = 'your-react-app-cache-v1';
+const urlsToCache = [
+  '/', // Cache the root page
+  '/index.html', // Cache the entry point of your app
+  '/static/js/main.chunk.js',
+  '/static/js/0.chunk.js',
+  '/static/js/bundle.js',
+  '/static/css/main.chunk.css',
+  // Add other URLs to cache as needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request)
+          .then(response => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          })
+          .catch(error => {
+            // Handle fetch errors, e.g., show a fallback page
+            console.error('Fetch error:', error);
+          });
+      })
+  );
+});
