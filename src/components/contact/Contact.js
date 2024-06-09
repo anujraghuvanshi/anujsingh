e536b4b9839d61f55cce394f55581be78824d9b0
Anuj Raghuvanshi <22232709+anujraghuvanshi@users.noreply.github.com>
2024-06-09 15:29:49 +0530

    Removed Unwanted Import

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,7 +5,6 @@ import { Bounce } from 'react-reveal'
 import ContactForm from '../contactForm/ContactForm'
 import linkedin from '../../images/social/linkedin.png'
 import github from '../../images/social/github.png'
-// import facebook from '../../images/social/facebook.png'
 import twitter from '../../images/social/twitter.png'
 import Stackoverflow from '../../images/social/stackoverflow.png'
 
@@ -22,7 +21,7 @@ const Contact = () => {
           >
             <img src={github} alt="Github Logo" width="40px" />
           </a>
-          
+
           <a
             href="https://stackoverflow.com/users/7078456/anuj-raghuvanshi"
             target="_blank"
